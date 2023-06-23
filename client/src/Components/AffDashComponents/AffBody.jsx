04408af0cf8d122de37312c660b9a1fd3e83253c
DiamondOreDev <harsh.diamondore@gmail.com>
2023-06-23 17:50:26 +0530

    Major Changes in Auths

---
+++ client/src/Components/AffDashComponents/AffBody.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AffBody = () => {
+  return (
+    <section>
+      <div className="w-max-auto flex items-center mt-10">
+        <h2 className="text-6xl font-semibold text-gray-100 px-10 py-10">
+          Refer More, Earn More
+        </h2>
+        <div>
+            <img src="" alt="" />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AffBody
