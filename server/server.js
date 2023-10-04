aeca46a78126b10d3d9c83edeb5c85a4e80b2b33
DiamondOreDev <harsh.diamondore@gmail.com>
2023-10-04 11:12:29 +0530

    IP

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ app.get('/', (req, res) => {
 })
 
 // Start the server
-app.listen(8080, "52.41.36.82", () => {
+app.listen(8080, "54.191.253.12", () => {
   console.log(`Server running on ${process.env.IP}:8080`);
 });
 
